d964b6001770cdff231d8e5749ebd6dfc90904b5
Alex Dietz <adietz@thehumaneleague.org>
2025-07-20 13:13:57 -0400

    Add button to switch video

--- src/Video/Video.component.tsx
+++ src/Video/Video.component.tsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import * as Styled from './Video.styles';
 import { videos } from './Video.data';
 
-const getRandomUrl = () =>
-  videos[Math.floor(Math.random() * videos.length)].url;
+const getRandomUrl = (url?: string) => {
+  const newUrl = videos[Math.floor(Math.random() * videos.length)].url;
+  if (newUrl === url) {
+    return getRandomUrl(url);
+  }
+  return newUrl;
+};
 
-const Video = () => (
-  <>
-    <Styled.Iframe src={getRandomUrl()} allowFullScreen />
-  </>
-);
+const Video = () => {
+  const [videoUrl, setVideoUrl] = useState(getRandomUrl());
+
+  return (
+    <>
+      <Styled.Iframe src={videoUrl} allowFullScreen />
+      <button onClick={() => setVideoUrl(getRandomUrl(videoUrl))}>
+        Switch Video
+      </button>
+    </>
+  );
+};
 
 export default Video;
